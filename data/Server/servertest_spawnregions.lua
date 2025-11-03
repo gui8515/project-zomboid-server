9e7e98a0397bfcae668c594815c1626bae565272
Guilherme Gois Pereira <gui8515@gmail.com>
2025-11-03 20:40:49 -0300

    .

--- data/Server/servertest_spawnregions.lua
+++ data/Server/servertest_spawnregions.lua
@@ -1,8 +1,9 @@
 function SpawnRegions()
 	return {
-		{ name = "2. Muldraugh - tranquilo, ideal para iniciantes.", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
-		{ name = "3. Riverside - tranquilo, bom loot e zumbis equilibrados.", file = "media/maps/Riverside, KY/spawnpoints.lua" },
-		{ name = "4. Rosewood - moderado, bom loot e zumbis equilibrados.", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
-		{ name = "5. West Point - desafiador, mais loot, mas muitos zumbis.", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "1. Muldraugh - Fácil, ideal para iniciantes.", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "2. Riverside - Tranquilo, bom loot e zumbis equilibrados.", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "3. Rosewood - Moderado, bom loot e zumbis equilibrados.", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "4. West Point - Desafiador, mais loot, mas muitos zumbis.", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "5. Aleatório - Loot variado e zumbis imprevisíveis.", file = "media/maps/Pillows All Spawns No LV/spawnpoints.lua" },
 	}
 end
